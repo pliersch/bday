f74197ce396875a629248b03f46bb87ba953f947
pliersch <hourby@gmail.com>
2015-11-18 15:54:20 +0100

    WIP

--- app/src/main/java/de/liersch/android/bday/widget/service/CalendarUtil.java
+++ app/src/main/java/de/liersch/android/bday/widget/service/CalendarUtil.java
@@ -1,21 +1,69 @@
 package de.liersch.android.bday.widget.service;
 
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class CalendarUtil {
 
   private static CalendarUtil mInstance;
 
-  public static CalendarUtil getInstance(){
+  public static CalendarUtil getInstance() {
     if (mInstance == null) {
       mInstance = new CalendarUtil();
     }
     return mInstance;
   }
 
-  public int getDaysLeftToBDay(String date) {
+  public int getDaysLeft(Calendar from, Calendar to) {
+    long diff = to.getTime().getTime() - from.getTime().getTime();
+    long convert = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    return (int) convert;
+  }
+
+  public int[] getMonthAndDaysLeft(Calendar from, Calendar to) {
+
+    int diffYear = to.get(Calendar.YEAR) - from.get(Calendar.YEAR);
+    int diffMonth = diffYear * 12 + to.get(Calendar.MONTH) - from.get(Calendar.MONTH);
+
+    int fullMonth = diffMonth > 0 ? diffMonth - 1 : 0;
+
+    int numDaysThisMonth = from.getActualMaximum(Calendar.DATE);
+    numDaysThisMonth -= from.get(Calendar.DATE);
+    int numDaysThisBirthdayMonth = to.get(Calendar.DATE);
+    int numDaysLeft = numDaysThisMonth + numDaysThisBirthdayMonth;
+
+    int months = Math.ceil(numDaysLeft / 30.5);
+    int days = numDaysLeft - 30;
+
+    months += fullMonth;
+
+    int[] result = new int[2];
+    result[0] = months;
+    result[1] = days;
+    return result;
+  }
+
+  public Calendar computeNextPossibleEvent(Calendar event, Calendar from) {
+    int year = from.get(Calendar.YEAR);
+    if(from.get(Calendar.MONTH) > event.get(Calendar.MONTH)) {
+      year++;
+    } else if(from.get(Calendar.MONTH) == event.get(Calendar.MONTH)) {
+      if(from.get(Calendar.DATE) > event.get(Calendar.DATE)) {
+        year++;
+      }
+    }
+    event.set(Calendar.YEAR, year);
+    return event;
+  }
+
+  public Calendar toCalendar(String string) {
+    String[] split = string.split("-");
+    int year = Integer.parseInt(split[0]);
+    int month = Integer.parseInt(split[1]);
+    int day = Integer.parseInt(split[2]);
     Calendar calendar = Calendar.getInstance();
-    return 0;
+    calendar.set(year, month, day, 0, 0);
+    return calendar;
   }
 
 }
---
+++ app/src/test/java/de/liersch/android/bday/widget/service/CalendarUtilTest.java
@@ -0,0 +1,131 @@
+package de.liersch.android.bday.widget.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.Exception;
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+public class CalendarUtilTest {
+
+  private CalendarUtil mUtil;
+  
+  @Before
+  public void setUp() throws Exception {
+    mUtil = CalendarUtil.getInstance();
+  }
+
+  //----------------------------------
+  //
+  // toCalendar
+  //
+  //----------------------------------
+
+  @Test
+  public void test_toCalendar() throws Exception {
+    Calendar result = mUtil.toCalendar("2000-01-01");
+    assertEquals(2000, result.get(Calendar.YEAR));
+    assertEquals(1, result.get(Calendar.MONTH));
+    assertEquals(1, result.get(Calendar.DATE));
+  }
+
+  //----------------------------------
+  //
+  // computeNextPossibleEvent
+  //
+  //----------------------------------
+
+  @Test
+  public void test_computeNextPossibleEvent_next_month() throws Exception {
+    Calendar event = getCalendar(1977, 3, 1);
+    Calendar from = getCalendar(2000, 2, 1);
+    Calendar result = mUtil.computeNextPossibleEvent(event, from);
+    assertEquals(2000, result.get(Calendar.YEAR));
+    assertEquals(3, result.get(Calendar.MONTH));
+    assertEquals(1, result.get(Calendar.DATE));
+  }
+
+  @Test
+  public void test_computeNextPossibleEvent_same_month_day_before() throws Exception {
+    Calendar event = getCalendar(1977, 3, 2);
+    Calendar from = getCalendar(2000, 3, 1);
+    Calendar result = mUtil.computeNextPossibleEvent(event, from);
+    assertEquals(2000, result.get(Calendar.YEAR));
+    assertEquals(3, result.get(Calendar.MONTH));
+    assertEquals(2, result.get(Calendar.DATE));
+  }
+
+  @Test
+  public void test_computeNextPossibleEvent_same_month_day_after() throws Exception {
+    Calendar event = getCalendar(1977, 3, 1);
+    Calendar from = getCalendar(2000, 3, 2);
+    Calendar result = mUtil.computeNextPossibleEvent(event, from);
+    assertEquals(2001, result.get(Calendar.YEAR));
+    assertEquals(3, result.get(Calendar.MONTH));
+    assertEquals(1, result.get(Calendar.DATE));
+  }
+
+  @Test
+  public void test_computeNextPossibleEvent_same_month_same_day() throws Exception {
+    Calendar event = getCalendar(1977, 3, 1);
+    Calendar from = getCalendar(2000, 3, 1);
+    Calendar result = mUtil.computeNextPossibleEvent(event, from);
+    assertEquals(2000, result.get(Calendar.YEAR));
+    assertEquals(3, result.get(Calendar.MONTH));
+    assertEquals(1, result.get(Calendar.DATE));
+  }
+
+  //----------------------------------
+  //
+  // getDaysLeft
+  //
+  //----------------------------------
+
+  @Test
+  public void test_getDaysLeft_first_to_last_day_of_year() throws Exception {
+    Calendar from = getCalendar(2015, 0, 1);
+    Calendar to = getCalendar(2015, 11, 31);
+    int daysLeft = mUtil.getDaysLeft(from, to);
+    int days = to.get(Calendar.DAY_OF_YEAR);
+    assertEquals(days - 1, daysLeft);
+  }
+
+  @Test
+  public void test_getDaysLeft_month_to_next_month() throws Exception {
+    Calendar from = getCalendar(2015, 0, 1);
+    Calendar to = getCalendar(2015, 1, 1);
+    int daysLeft = mUtil.getDaysLeft(from, to);
+    assertEquals(31, daysLeft);
+  }
+
+  //----------------------------------
+  //
+  // getMonthAndDaysLeft
+  //
+  //----------------------------------
+
+
+  @Test
+  public void test_getMonthAndDaysLeft_first_to_last_day_of_year() throws Exception {
+    Calendar from = getCalendar(2015, 0, 1);
+    Calendar to = getCalendar(2015, 11, 31);
+    int[] result = mUtil.getMonthAndDaysLeft(from, to);
+    //int days = to.get(Calendar.DAY_OF_YEAR);
+    assertEquals(11, result[0]);
+  }
+
+  //----------------------------------
+  //
+  // HELPER
+  //
+  //----------------------------------
+
+  private Calendar getCalendar(int year, int month, int day) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(year, month, day);
+    return calendar;
+  }
+
+}
