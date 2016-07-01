30ddf463ab69c0512bb50cefa20a17099e9d82dc
pliersch <hourby@gmail.com>
2016-07-01 14:36:31 +0200

    Clean up

--- app/src/main/java/de/liersch/android/bday/app/DetailActivity.java
+++ app/src/main/java/de/liersch/android/bday/app/DetailActivity.java
@@ -1,45 +1,26 @@
 package de.liersch.android.bday.app;
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
+import android.content.ContentResolver;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.transition.Slide;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-
 import de.liersch.android.bday.R;
-import de.liersch.android.bday.app.util.ViewModel;
+import de.liersch.android.bday.db.ContactUtil;
+import de.liersch.android.bday.ui.contacts.ContactListFragment;
 
 public class DetailActivity extends AppCompatActivity {
 
   private static final String EXTRA_IMAGE = "com.antonioleiva.materializeyourapp.extraImage";
-  private static final String EXTRA_TITLE = "com.antonioleiva.materializeyourapp.extraTitle";
   private CollapsingToolbarLayout collapsingToolbarLayout;
-
-  public static void navigate(AppCompatActivity activity, View transitionImage, ViewModel viewModel) {
-    Intent intent = new Intent(activity, DetailActivity.class);
-    intent.putExtra(EXTRA_IMAGE, viewModel.getImage());
-    intent.putExtra(EXTRA_TITLE, viewModel.getText());
-
-    ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(activity, transitionImage, EXTRA_IMAGE);
-    ActivityCompat.startActivity(activity, intent, options.toBundle());
-  }
 
   @SuppressWarnings("ConstantConditions")
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -53,29 +34,21 @@ public class DetailActivity extends AppCompatActivity {
     setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-    String itemTitle = getIntent().getStringExtra(EXTRA_TITLE);
+    final ImageView imageView = (ImageView) findViewById(R.id.image);
+    String name = getIntent().getStringExtra(ContactListFragment.NAME);
+    long contactId = Long.parseLong(getIntent().getStringExtra(ContactListFragment.CONTACT_ID));
+    final ContentResolver contentResolver = getApplicationContext().getContentResolver();
+    Bitmap bitmap = ContactUtil.getInstance().loadContactPhoto(contentResolver, contactId);
+    if (bitmap != null) {
+      imageView.setImageBitmap(bitmap);
+    }
+
     collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-    collapsingToolbarLayout.setTitle(itemTitle);
+    collapsingToolbarLayout.setTitle(name);
     collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
 
-    final ImageView image = (ImageView) findViewById(R.id.image);
-    Picasso.with(this).load(getIntent().getStringExtra(EXTRA_IMAGE)).into(image, new Callback() {
-      @Override public void onSuccess() {
-        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-          public void onGenerated(Palette palette) {
-            applyPalette(palette);
-          }
-        });
-      }
-
-      @Override public void onError() {
-
-      }
-    });
-
     TextView title = (TextView) findViewById(R.id.title);
-    title.setText(itemTitle);
+    title.setText("Title");
   }
 
   @Override public boolean dispatchTouchEvent(MotionEvent motionEvent) {
@@ -93,22 +66,5 @@ public class DetailActivity extends AppCompatActivity {
       getWindow().setEnterTransition(transition);
       getWindow().setReturnTransition(transition);
     }
-  }
-
-  private void applyPalette(Palette palette) {
-    int primaryDark = getResources().getColor(R.color.primary_dark);
-    int primary = getResources().getColor(R.color.primary);
-    collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
-    collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
-    updateBackground((FloatingActionButton) findViewById(R.id.fab), palette);
-    supportStartPostponedEnterTransition();
-  }
-
-  private void updateBackground(FloatingActionButton fab, Palette palette) {
-    int lightVibrantColor = palette.getLightVibrantColor(getResources().getColor(android.R.color.white));
-    int vibrantColor = palette.getVibrantColor(getResources().getColor(R.color.accent));
-
-    fab.setRippleColor(lightVibrantColor);
-    fab.setBackgroundTintList(ColorStateList.valueOf(vibrantColor));
   }
 }
