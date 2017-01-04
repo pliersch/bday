665da2816225c5f1617e6122a5d651baf8642064
pliersch <hourby@gmail.com>
2017-01-04 20:35:01 +0100

    Revert

--- app/src/main/java/de/liersch/android/bday/beans/Contact.java
+++ app/src/main/java/de/liersch/android/bday/beans/Contact.java
@@ -2,17 +2,15 @@ package de.liersch.android.bday.beans;
 
 public class Contact {
 
-  public int position;
   public String bday;
   public String name;
   public long userID;
   public boolean notified;
 
-  public Contact(long userID, String name, String bday, boolean notified, int position) {
+  public Contact(long userID, String name, String bday, boolean notified) {
     this.userID = userID;
     this.name = name;
     this.bday = bday;
     this.notified = notified;
-    this.position = position;
   }
 }
