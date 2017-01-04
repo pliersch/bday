05f029617aaa141fa22aea492fdb159b41e97120
pliersch <hourby@gmail.com>
2017-01-04 15:47:23 +0100

    Add property

--- app/src/main/java/de/liersch/android/bday/beans/Contact.java
+++ app/src/main/java/de/liersch/android/bday/beans/Contact.java
@@ -2,15 +2,17 @@ package de.liersch.android.bday.beans;
 
 public class Contact {
 
+  public int position;
   public String bday;
   public String name;
   public long userID;
   public boolean notified;
 
-  public Contact(long userID, String name, String bday, boolean notified) {
+  public Contact(long userID, String name, String bday, boolean notified, int position) {
     this.userID = userID;
     this.name = name;
     this.bday = bday;
     this.notified = notified;
+    this.position = position;
   }
 }
