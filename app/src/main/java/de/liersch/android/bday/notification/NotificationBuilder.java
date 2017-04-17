5fc8355557f0b60adcffbb26fce698d54a34d02f
pliersch <hourby@gmail.com>
2017-04-17 17:59:55 +0200

    Remove obsolete code

--- app/src/main/java/de/liersch/android/bday/notification/NotificationBuilder.java
+++ app/src/main/java/de/liersch/android/bday/notification/NotificationBuilder.java
@@ -4,9 +4,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
-import de.liersch.android.bday.R;
 import de.liersch.android.bday.activity.MainActivity;
 import de.liersch.android.bday.db.ContactUtil;
 
@@ -19,11 +17,6 @@ public abstract class NotificationBuilder {
   }
 
   protected Bitmap getIcon(long userID, Context applicationContext) {
-    Bitmap bitmap = ContactUtil.getInstance().loadContactPhoto(applicationContext.getContentResolver(), userID);
-    if (bitmap == null) {
-      // TODO: why share icon not shown
-      bitmap = BitmapFactory.decodeResource(applicationContext.getResources(), R.drawable.ic_menu_share);
-    }
-    return bitmap;
+    return ContactUtil.getInstance().loadContactPhoto(applicationContext.getContentResolver(), userID);
   }
 }
